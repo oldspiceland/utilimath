712a81572579dcf9114178bae1f5faadb2f17aa8
George Spiceland <georgespiceland@gmail.com>
2016-08-14 19:20:14 -0400

    Renamed the function and associated tests to better reprsent the variable type

--- utilimath.go
+++ utilimath.go
@@ -3,7 +3,7 @@ package utilimath
 import ()
 
 //IntPow is an Integer-only version of math.Pow
-func IntPow(base, exp uint64) (powered uint64) {
+func UInt64Pow(base, exp uint64) (powered uint64) {
 	powered = base
 	for i := uint64(1); i < exp; i++ {
 		powered = powered * base
--- utilimath_test.go
+++ utilimath_test.go
@@ -4,19 +4,19 @@ import (
 	"testing"
 )
 
-func TestIntPow10to2(t *testing.T) {
+func TestUInt64Pow10to2(t *testing.T) {
 	base := uint64(10)
 	exp := uint64(2)
-	powered := IntPow(base, exp)
+	powered := UInt64Pow(base, exp)
 	if powered != uint64(100) {
 		t.Error("Expected 100, got ", powered)
 	}
 }
 
-func TestIntPow15to3(t *testing.T) {
+func TestUInt64Pow15to3(t *testing.T) {
 	base := uint64(15)
 	exp := uint64(3)
-	powered := IntPow(base, exp)
+	powered := UInt64Pow(base, exp)
 	if powered != uint64(3375) {
 		t.Error("Expected 3375, got ", powered)
 	}
