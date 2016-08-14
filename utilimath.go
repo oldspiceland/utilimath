79e0cd54b5ba7aff3f5080e0e5393b05cd00a764
George Spiceland <georgespiceland@gmail.com>
2016-08-14 19:16:55 -0400

    Fixed a lamebrained math mistake

--- utilimath.go
+++ utilimath.go
@@ -4,9 +4,9 @@ import ()
 
 //IntPow is an Integer-only version of math.Pow
 func IntPow(base, exp uint64) (powered uint64) {
-	for i := uint64(1); i <= exp; i++ {
-		base = base + base
-	}
 	powered = base
+	for i := uint64(1); i <= exp; i++ {
+		powered = powered * base
+	}
 	return
 }
