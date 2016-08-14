2fd206c83e1f20f6ddb078861406837f51569bed
George Spiceland <georgespiceland@gmail.com>
2016-08-14 19:18:34 -0400

    I think I fixed an error with too many exponents...

--- utilimath.go
+++ utilimath.go
@@ -5,7 +5,7 @@ import ()
 //IntPow is an Integer-only version of math.Pow
 func IntPow(base, exp uint64) (powered uint64) {
 	powered = base
-	for i := uint64(1); i <= exp; i++ {
+	for i := uint64(1); i < exp; i++ {
 		powered = powered * base
 	}
 	return
